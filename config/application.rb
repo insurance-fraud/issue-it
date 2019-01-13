f62322870914c0ac09546364dde9b858757f799b
Nikola Đuza <nikolaseap@gmail.com>
2019-01-13 13:14:11 +0100

    Log to standard output

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module IssueIt
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Log to standard out
+    config.logger = Logger.new(STDOUT)
   end
 end
