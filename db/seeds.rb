d1c674366495def24b0570875adb225f3d798ccb
Nikola Đuza <nikolaseap@gmail.com>
2019-06-29 23:48:41 -0500

    Add account with no money

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 User.create(pan: 2, card_holder_name: 2, expiry_date: 2, security_code: 2, reserved_balance: 0, total_balance: 100000)
 User.create(pan: 3, card_holder_name: 3, expiry_date: 3, security_code: 3, reserved_balance: 0, total_balance: 200000)
+User.create(pan: 4, card_holder_name: 4, expiry_date: 4, security_code: 4, reserved_balance: 0, total_balance: 1000)
